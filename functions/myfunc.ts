a6d2ddb7a0bf6b64e4abbc3edad62f4b64c3462c
Dave New <sanodn@gmail.com>
2026-05-19 14:43:47 +0200

    Reporting test

---
+++ functions/myfunc.ts
@@ -0,0 +1,6 @@
+import { Myfunc } from '@teamkeel/sdk';
+
+// To learn more about what you can do with custom functions, visit https://docs.keel.so/functions
+export default Myfunc(async (ctx, inputs) => {
+    return {};
+});
