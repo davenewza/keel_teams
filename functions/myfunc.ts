cdd04198310dae28bbd31063575585c947167373
Dave New <sanodn@gmail.com>
2026-06-26 13:03:54 +0200

    removed template

--- functions/myfunc.ts
+++ functions/myfunc.ts
@@ -11,22 +11,23 @@ const templateHtml = readFileSync(
 // To learn more about what you can do with custom functions, visit https://docs.keel.so/functions
 export default Myfunc(async (ctx, inputs) => {
 
+    // await notify.email({
+    //     recipients: { to: { teams: [Team.Accounts] }, cc: { emails: "sanodn@gmail.com" }, bcc: { identities: ctx.identity } },
+    //     subject: "Hello world",
+    //     content: {
+    //         title: "Hello world!",
+    //         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    //         actions: [
+    //             { label: "Go", url: "https://keel.so" },
+    //             { label: "Somethig else", url: "https://keel.so/help" }
+    //         ]
+    //     }
+    // });
+
+
     await notify.email({
+        //recipients: { to: { emails: ["dave.new@keel.xyz", "nobody.here@keel.xyz"] } },
         recipients: { to: { teams: [Team.Accounts] }, cc: { emails: "sanodn@gmail.com" }, bcc: { identities: ctx.identity } },
-        subject: "Hello world",
-        content: {
-            title: "Hello world!",
-            body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            actions: [
-                { label: "Go", url: "https://keel.so" },
-                { label: "Somethig else", url: "https://keel.so/help" }
-            ]
-        }
-    });
-
-
-    await notify.email({
-        recipients: { to: { emails: ["dave.new@keel.xyz", "nobody.here@keel.xyz"] } },
         subject: "Hello world",
         content: templateHtml
     });
