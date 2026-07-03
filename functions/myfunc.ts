7a384b9d17fc7e55878a7940dde52037d001812d
Dave New <sanodn@gmail.com>
2026-07-03 14:54:09 +0200

    clean up

--- functions/myfunc.ts
+++ functions/myfunc.ts
@@ -1,42 +1,23 @@
-import { readFileSync } from 'node:fs';
-import { join, dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
+
 import { Myfunc, notify, Team } from '@teamkeel/sdk';
 
-// const templateHtml = readFileSync(
-//     join(dirname(fileURLToPath(import.meta.url)), 'mytemplate.html'),
-//     'utf-8'
-// );
-
-// To learn more about what you can do with custom functions, visit https://docs.keel.so/functions
 export default Myfunc(async (ctx, inputs) => {
 
-    // await notify.email({
-    //     recipients: { to: { teams: [Team.Accounts] }, cc: { emails: "sanodn@gmail.com" }, bcc: { identities: ctx.identity } },
-    //     subject: "Hello world",
-    //     content: {
-    //         title: "Hello world!",
-    //         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    //         actions: [
-    //             { label: "Go", url: "https://keel.so" },
-    //             { label: "Somethig else", url: "https://keel.so/help" }
-    //         ]
-    //     }
-    // });
-
-
     await notify.email({
-        //recipients: { to: { emails: ["dave.new@keel.xyz", "nobody.here@keel.xyz"] } },
-        recipients: { to: { teams: [Team.Accounts] }, cc: { emails: "sanodn@gmail.com" }, bcc: { identities: ctx.identity } },
-
-        subject: "Hello world!!",
+        recipients: {
+            to: {
+                teams: [Team.Accounts],
+                emails: "sanodn@gmail.com",
+                identities: ctx.identity
+            },
+        },
+        subject: "You have mail!",
         content: {
-            body: "Hello again 5", actions: [
-                { label: "Go", url: "https://keel.so" },
-                { label: "Somethig else", url: "https://keel.so/help" }
+            body: "Hello world", actions: [
+                { label: "Go to Keel", url: "https://keel.so" },
+                { label: "View docs", url: "https://keel.so/docs" }
             ]
         }
-
     });
 
     return {};
